21214d96d9d41f2e8067bf932582fd1807d14bc0
dm0817 <daniel.martinarenas@soprasteria.com>
2020-03-01 01:58:12 +0100

    yup

--- cli/commands/test.py
+++ cli/commands/test.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import Tuple
+from typing import Tuple, Iterable
 import click
 import ast
 from eos import eos_client, eos_key, ADMIN, open_req, show_transaction_response, upload_req
@@ -14,16 +14,13 @@ def test() -> None:
     pass
 
 
-def _get_request_form() -> Tuple[OptionList, ...]:
-    """Retrieves input data from user in order to set or config a new list."""
-    lists = eos_client.get_table('hemerton', 'hemerton', 'lists', limit=99999)['rows']
-    lists_prompt = [str(row['numlist']) for row in lists]
-
+def _get_request_form(lists: Iterable[str]) -> Tuple[OptionList, ...]:
+    """Retrieves input data from user in order to send a new request."""
     return (
         OptionList(
             "numlist",
             message="Selecciona una lista para la petición: ",
-            choices=lists_prompt,
+            choices=lists,
         ), )
 
 
@@ -55,8 +52,16 @@ def _get_last_request_data() -> int:
 @test.command()
 def new() -> None:
     """Sends a new request to the Smart Contract"""
+    # Retrieve existing lists
+    lists = eos_client.get_table('hemerton', 'hemerton', 'lists', limit=99999)['rows']
+    if not lists:
+        print('At least a list is meant to be registered before send a request.')
+        print('Try using the following command: hemerton lists new.')
+        print('Exiting...')
+        return
+    lists_prompt = [str(row['numlist']) for row in lists]
     # Request user data
-    arguments = prompt(_get_request_form())
+    arguments = prompt(_get_request_form(lists_prompt))
     # Cast strings to corresponding types
     arguments = {k: ast.literal_eval(v) for k, v in arguments.items()}
     # Add admin user to request
